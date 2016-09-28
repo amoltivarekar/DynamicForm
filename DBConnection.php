22608b9e5c3c2d06b8114eb08dbb2004e7147020
Amol Tivarekar <amoltivarekar@gmail.com>
2016-09-28 21:50:10 +0530

    Create DBConnection.php

---
+++ DBConnection.php
@@ -0,0 +1,17 @@
+<?php
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$dbname="MYDB";
+
+	$conn=new mysqli($servername,$username,$password,$dbname);
+
+	if(!$conn)
+	{
+		echo "<h3>No connection between Database and User </h3>";
+	}
+	else
+	{
+		echo "<h3>Connection Successfully :) </h3>";
+	}
+?>
